429e93caec33b40421ff6a42bf6a68c4ac5fff71
johntatum <johntatum@a2ca494f-d447-4333-81f5-3eee1eec23e8>
2016-06-16 02:09:29 +0000

    Added ttb, created project shell.

---
+++ trunk/src/net/scientifichooliganism/jsonplugin/JSONPlugin.java
@@ -0,0 +1,12 @@
+package net.scientifichooliganism.jsonplugin;
+
+import net.scientifichooliganism.javaplug.interfaces.Plugin;
+import net.scientifichooliganism.javaplug.vo.*;
+
+import net.scientifichooliganism.jsonplugin.bindings.*;
+
+public class JSONPlugin implements Plugin {
+	JSONPlugin () {
+		//
+	}
+}
