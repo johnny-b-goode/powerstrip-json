c5a8f4cf92a9836431a4a8c226cc78d1b5dbd73e
tylerhatwigcgi <tylerhatwigcgi@a2ca494f-d447-4333-81f5-3eee1eec23e8>
2016-08-29 13:51:00 +0000

    Remove pretty printing on json and format/fix tests

--- trunk/tst/net/scientifichooliganism/jsonplugin/JSONPluginTest.java
+++ trunk/tst/net/scientifichooliganism/jsonplugin/JSONPluginTest.java
@@ -5,24 +5,28 @@ import net.scientifichooliganism.javaplug.interfaces.MetaData;
 import net.scientifichooliganism.javaplug.interfaces.Task;
 import net.scientifichooliganism.javaplug.vo.BaseAction;
 import net.scientifichooliganism.javaplug.vo.BaseMetaData;
-import net.scientifichooliganism.javaplug.vo.BaseTask;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class JSONPluginTest {
     private JSONPlugin plugin;
+    private DateFormat sdf;
 
     public JSONPluginTest () {
         plugin = null;
+        sdf = null;
     }
 
     @Before
     public void init () {
         plugin = new JSONPlugin();
+        sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss+Z");
     }
 
     @Test
@@ -32,43 +36,88 @@ public class JSONPluginTest {
 
     @Test
     public void test01 () {
-        JSONPlugin plugin = new JSONPlugin();
+        String metaDataKey = "some_key";
+        String metaDataValue = "some_value";
+        String metaDataID = "12";
+        String metaDataObject = "some_object";
+        String metaDataObjectID = "254";
+        String metaDataLabel = "some_label";
+
         MetaData data1 = new BaseMetaData();
-        MetaData data2 = new BaseMetaData();
-        data1.setKey("key1");
-        data2.setKey("key2");
-        data1.setValue("value1");
-        data2.setValue("value2");
+        data1.setKey(metaDataKey);
+        data1.setValue(metaDataValue);
+        data1.setID(metaDataID);
+        data1.setObject(metaDataObject);
+        data1.setObjectID(metaDataObjectID);
+        data1.setLabel(metaDataLabel);
 
-        data1.setID("12");
-        data1.setObject("object");
-        data1.setObjectID("542");
-        data1.setLabel("label");
+        String actionId = String.valueOf(42);
+        String actionLabel = "some_label";
+        String actionDescription = "some_description";
+        String actionModule = "some_module";
+        String actionClass = "some_class";
+        String actionURL = "some_url";
+        String actionMethod = "some_method";
+
         Action action = new BaseAction();
-        action.setID(String.valueOf(42));
-        action.setLabel("Default Label");
-        action.setDescription("This is my description");
-        action.setModule("Awesome module!");
-        action.setKlass("Class of 2016");
-        action.setURL("www.google.com");
-        action.setMethod("myMethod");
+        action.setID(actionId);
+        action.setLabel(actionLabel);
+        action.setDescription(actionDescription);
+        action.setModule(actionModule);
+        action.setKlass(actionClass);
+        action.setURL(actionURL);
+        action.setMethod(actionMethod);
         action.addMetaData(data1);
-        action.addMetaData(data2);
 
-        Task testTask = new BaseTask();
-        testTask.setID("0");
-        testTask.setStartDate(new Date());
+        String expectedResult = "{\"action\":" +
+                "{\"description\":\"" + actionDescription + "\"," +
+                "\"module\":\"" + actionModule + "\"," +
+                "\"class\":\"" + actionClass + "\"," +
+                "\"url\":\"" + actionURL + "\"," +
+                "\"method\":\"" + actionMethod + "\"," +
+                "\"id\":\"" + actionId + "\"," +
+                "\"label\":\"" + actionLabel + "\"," +
+                "\"metadata\":" +
+                "[" +
+                "{\"object\":\"" + metaDataObject + "\"," +
+                "\"object_id\":\"" + metaDataObjectID + "\"," +
+                "\"key\":\"" + metaDataKey + "\"," +
+                "\"value\":\"" + metaDataValue + "\"," +
+                "\"id\":\"" + metaDataID + "\"," +
+                "\"label\":\"" + metaDataLabel + "\"}" +
+                "]" +
+                "}}";
 
-        String json = plugin.jsonFromObject(testTask);
-        System.out.println(json);
+        assertEquals(expectedResult, plugin.jsonFromObject(action));
 
-        json = "{\"task\":{\"name\":\"test for dateeeeeeeee\",\"startDate\":\"2016-08-10T05:00:00+-0500\",\"completedDate\":\"2016-08-11T05:00:00+-0500\",\"metadata\":[]}}";
+    }
 
+    @Test
+    public void test02(){
+        String taskName = "some_name";
+        String taskStartDateString = "2016-08-10T05:00:00+-0500";
+        Date taskStartDate = null;
+        String taskCompletedDateString = "2016-08-11T05:00:00+-0500";
+        Date taskCompletedDate = null;
+        String json = "{\"task\":" +
+                "{\"name\":\"" + taskName + "\"," +
+                "\"start_date\":\"" + taskStartDateString + "\"," +
+                "\"completed_date\":\"" + taskCompletedDateString + "\"," +
+                "\"metadata\":[]}}";
+
+        try {
+            taskStartDate = sdf.parse(taskStartDateString);
+            taskCompletedDate = sdf.parse(taskCompletedDateString);
+        } catch (Exception exc){
+            exc.printStackTrace();
+            fail("Error parsing date strings!\nException: " + exc.getClass().getSimpleName() + "\nMessage: " + exc.getMessage());
+        }
 
         Task object = (Task)plugin.objectFromJson(json);
 
-
-        System.out.println(json);
-        System.out.println(object);
+        assertEquals(taskName, object.getName());
+        assertEquals(taskStartDate, object.getStartDate());
+        assertEquals(taskCompletedDate, object.getCompletedDate());
+        assert(object.getMetaData().isEmpty());
     }
 }
